91ce8628c425f96125afc31d0a43b623e101ee49
Mevludin Arnautovic <marnaut92@gmail.com>
2021-01-18 15:28:01 +0100

    first init

---
+++ src/main/java/com/mevludin/APITouristBoard/ApiTouristBoardApplication.java
@@ -0,0 +1,13 @@
+package com.mevludin.APITouristBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTouristBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTouristBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mevludin/APITouristBoard/ApiTouristBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mevludin.APITouristBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTouristBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
